61041efc1147203700e68c88a8b7e65bcda2d313
bharathGuna <bharathguna610@gmail.com>
2016-11-21 19:57:50 -0700

    working on the nav bar

--- inkweaver/app/component/app.component.ts
+++ inkweaver/app/component/app.component.ts
@@ -2,13 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<nav>
-                <h3>Testing Navigation</h3>
-                 <a routerLink="/edit">Edit</a>
-                 <a routerLink="/wiki">Wiki</a>
-                 <a routerLink="/more-option">More Option</a>
-               </nav>
-                <router-outlet></router-outlet>
-              `
+    templateUrl: 'app/html/app.component.html',
+
 })
-export class AppComponent { }
+export class AppComponent {
+    title = "Test Navigation Bar"
+}
