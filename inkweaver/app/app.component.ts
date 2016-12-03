eb062d14389df49567cea6d9b3d24281f3b47ed6
bharathGuna <bharathguna610@gmail.com>
2016-12-03 16:15:10 -0700

    fixed the url

--- inkweaver/app/app.component.ts
+++ inkweaver/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/primeng';
-
+import { Router, NavigationStart, Event} from '@angular/router';
+import 'rxjs/add/operator/filter';
 @Component({
     selector: 'ink-app',
     templateUrl: './app/app.component.html'
@@ -8,13 +9,32 @@ import { MenuItem } from 'primeng/primeng';
 export class AppComponent {
     private title: string;
     private items: MenuItem[];
+    private activeItem: MenuItem;
 
-    constructor() {
+    constructor(private router: Router) {
         this.title = 'My Great Story';
         this.items = [
             { label: 'Edit', icon: 'fa-pencil-square-o', routerLink: ['/edit'] },
             { label: 'Wiki', icon: 'fa-book', routerLink: ['/wiki'] },
             { label: 'Settings', icon: 'fa-cogs', routerLink: ['/settings'] }
         ];
+
+        this.activeItem = this.items[0];
+
+
+        //this changes the navigation bar highlight
+        router.events
+            .filter(event => event instanceof NavigationStart)
+            .subscribe((event: Event) => {
+                if (event.url === '/' || event.url === '/edit' )
+                {
+                    this.activeItem = this.items[0];
+                }
+                else if (event.url === '/wiki')
+                    this.activeItem = this.items[1];
+                else if (event.url === '/settings')
+                    this.activeItem = this.items[2];
+            });
+         
     }
 }
