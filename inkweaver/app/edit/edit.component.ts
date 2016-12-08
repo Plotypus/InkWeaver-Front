a6bf948ae7239032414d144a71e49abea9b3aaf1
bharathGuna <bharathguna610@gmail.com>
2016-12-08 16:13:44 -0700

    Have chaged the CSS no working on the networking

--- inkweaver/app/edit/edit.component.ts
+++ inkweaver/app/edit/edit.component.ts
@@ -1,7 +1,10 @@
-﻿import { Component } from '@angular/core';
-import { MenuItem } from 'primeng/primeng';
+﻿import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { Editor } from 'primeng/primeng';
+import { MenuItem } from 'primeng/primeng';
 import { EditService } from './edit.service';
+import { WikiService } from '../wiki/wiki.service';
 import { ParserService } from '../shared/parser.service';
 
 @Component({
@@ -10,15 +13,37 @@ import { ParserService } from '../shared/parser.service';
 })
 export class EditComponent {
     private data: any;
+    @ViewChild(Editor) editor: Editor;
 
-    constructor(private editService: EditService, private parser: ParserService) {
+    constructor(
+        private router: Router,
+        private elRef: ElementRef,
+        private parser: ParserService,
+        private editService: EditService,
+        private wikiService: WikiService) {
+
         this.data = parser.data;
-        editService.getUserInfo();
+        parser.receive().subscribe((action: string) => {
+            if (action == 'load_chapter_with_paragraphs' || action == 'get_all_paragraphs')
+                this.clickableLinks();
+        });
+
+        if (this.data.story.id == '') {
+            editService.getUserInfo();
+        }
     }
 
     public switchChapter(event: any) {
         this.editService.loadChapterWithParagraphs(event.data.id);
     }
 
-
+    public clickableLinks() {
+        let threads: Element[] = this.elRef.nativeElement.querySelectorAll('a[href]');
+        for (let thread of threads) {
+            thread.addEventListener('click', (event: any) => {
+                event.preventDefault();
+                this.router.navigate(['/wiki']);
+            });
+        }
+    }
 }
