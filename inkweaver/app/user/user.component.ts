712d708d9ef1264e5f4e5f23a4a3a85ce2948676
Kyle <kyledpierson@gmail.com>
2017-01-20 11:51:11 -0700

    User page templated

--- inkweaver/app/user/user.component.ts
+++ inkweaver/app/user/user.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Header } from 'primeng/primeng';
 
 import { UserService } from './user.service';
 import { EditService } from '../story/edit/edit.service';
@@ -11,6 +12,7 @@ import { ParserService } from '../shared/parser.service';
 })
 export class UserComponent {
     private data: any;
+    private colors: string[];
 
     constructor(
         private router: Router,
@@ -20,11 +22,34 @@ export class UserComponent {
 
     ngOnInit() {
         this.data = this.parserService.data;
+
+        for (let i = 1; i <= 10; i++) {
+            this.data.stories.push({
+                title: 'Story ' + i,
+                story_id: i
+            });
+        }
+
+        this.colors = [
+            "#b0c9dd", // light blue
+            "#fdd17c", // yellow
+            "#74b0b8", // muted blue
+            "#acd8b4", // pastel green
+            "#903737", // maroon
+            "#8779c3", // purple
+            "#8c744e", // brown
+            "#cb735c", // red-orange
+            "#4d6b61" // green
+        ];
     }
 
     public selectStory(story_id: string) {
         this.editService.getStoryInformation(story_id);
         this.editService.getStoryHierarchy(story_id);
         this.router.navigate(['/story/edit']);
+    }
+
+    public randomColor(story_id: number) {
+        return this.colors[story_id % this.colors.length];
     }
 }
--- inkweaver/app/user/user.module.ts
+++ inkweaver/app/user/user.module.ts
@@ -1,12 +1,25 @@
 ﻿import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {
+    PanelModule,
+    ButtonModule,
+    CarouselModule,
+    DataGridModule
+} from 'primeng/primeng';
 
 import { UserService } from './user.service';
 import { UserComponent } from './user.component';
 
 @NgModule({
-    imports: [FormsModule, CommonModule],
+    imports: [
+        FormsModule,
+        CommonModule,
+        PanelModule,
+        ButtonModule,
+        CarouselModule,
+        DataGridModule
+    ],
     providers: [UserService],
     declarations: [UserComponent],
     bootstrap: [UserComponent]
