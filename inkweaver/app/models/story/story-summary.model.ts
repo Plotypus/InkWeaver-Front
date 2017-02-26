c92c3a7754573b52d059169a9ef68e586de82592
bharathGuna <bharathguna610@gmail.com>
2017-02-25 17:02:57 -0700

    Merging changes into Dev
    Include bug fixes and references and styling changes

--- inkweaver/app/models/story/story-summary.model.ts
+++ inkweaver/app/models/story/story-summary.model.ts
@@ -4,4 +4,5 @@ export class StorySummary {
     story_id: ID;
     title: string;
     access_level: string;
+    position_context: any;
 }
--- inkweaver/app/user/user.service.ts
+++ inkweaver/app/user/user.service.ts
@@ -2,6 +2,8 @@
 
 import { ApiService } from '../shared/api.service';
 
+import { ID } from '../models/id.model';
+
 @Injectable()
 export class UserService {
     constructor(private apiService: ApiService) { }
@@ -21,6 +23,43 @@ export class UserService {
     public getUserWikis() {
         this.apiService.send({
             action: 'get_user_wikis'
+        });
+    }
+
+    // Set User Information
+    public setUserName(name: string) {
+        this.apiService.send({
+            action: 'set_user_name',
+            name: name
+        });
+    }
+
+    public setUserEmail(email: string) {
+        this.apiService.send({
+            action: 'set_user_email',
+            email: email
+        });
+    }
+
+    public setUserBio(bio: string) {
+        this.apiService.send({
+            action: 'set_user_bio',
+            bio: bio
+        });
+    }
+
+    public setUserAvatar(avatar: string) {
+        this.apiService.send({
+            action: 'set_user_avatar',
+            avatar: avatar
+        });
+    }
+
+    public setUserStoryPositionContext(storyID: ID, sectionID: ID, paragraphID: ID) {
+        this.apiService.send({
+            action: 'set_user_story_position_context',
+            story_id: storyID,
+            position_context: { section_id: sectionID, paragraph_id: paragraphID }
         });
     }
 }
