6f55f76a9ae6181811341cfc01fb6050fb62ce9e
bharathGuna <bharathguna610@gmail.com>
2017-02-28 14:18:21 -0700

    adding stats files

--- inkweaver/app/story/stats/stats.service.ts
+++ inkweaver/app/story/stats/stats.service.ts
@@ -1,8 +1,1 @@
-﻿import { Injectable } from '@angular/core';
-
-import { ApiService } from '../../shared/api.service';
-
-@Injectable()
-export class SettingsService {
-    constructor(private parser: ApiService) { }
-}
+﻿
